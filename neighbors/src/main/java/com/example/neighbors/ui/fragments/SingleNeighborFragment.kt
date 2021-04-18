bec226d28b7b05801f3293f36e66f8626041ab97
Maeva Reynaud <maeva.reynaud@hotmail.fr>
2021-04-18 21:26:33 +0200

    TP4 - Step 5

--- neighbors/src/main/java/com/example/neighbors/ui/fragments/SingleNeighborFragment.kt
+++ neighbors/src/main/java/com/example/neighbors/ui/fragments/SingleNeighborFragment.kt
@@ -4,16 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.neighbors.R
-import com.example.neighbors.adapter.NeighborBindingAdapter
 import com.example.neighbors.databinding.NeighbourDetailBinding
 import com.example.neighbors.di.DI
 import com.example.neighbors.models.Neighbor
+import java.util.concurrent.Executors
 
 class SingleNeighborFragment(val neighbor: Neighbor) : androidx.fragment.app.Fragment() {
 
     private lateinit var binding: NeighbourDetailBinding
-    private var noPersistentNeighbor = neighbor
+    private var singleNeighbor = neighbor
+    var isFavorite: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,16 +23,31 @@ class SingleNeighborFragment(val neighbor: Neighbor) : androidx.fragment.app.Fra
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.neighbour_detail, container, false)
-        binding.neighbor = noPersistentNeighbor
+        binding.neighbor = singleNeighbor
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.neighborFavorite.setOnClickListener {
-            DI.repository.favoriteNeighbor(neighbor)
+            onAddFavorite(neighbor)
         }
     }
 
+    private fun onAddFavorite(neighbor: Neighbor) {
+        Executors.newSingleThreadExecutor().execute {
+            DI.repository.favoriteNeighbor(neighbor)
+        }
 
+        isFavorite = !isFavorite
+        changeFavoriteIcon()
+    }
+
+    private fun changeFavoriteIcon() {
+        if (isFavorite) {
+            binding.neighborFavorite.setImageResource(R.drawable.ic_baseline_favorite_24)
+        } else {
+            binding.neighborFavorite.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+        }
+    }
 }
